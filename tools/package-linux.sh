aa0f2e9bab48643b454f5681095dd0dcc88b9f3f
Alex Reinking <alex.reinking@gmail.com>
2020-07-22 14:36:54 -0700

    Fix Linux packaging script. Add Windows packaging script.

--- tools/package-linux.sh
+++ tools/package-linux.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DIR="$(readlink -f $DIR/..)"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DIR="$(readlink -f "$DIR/..")"
 
-cmake -G "Ninja Multi-Config" \
-      -DLLVM_DIR=$LLVM_DIR -DClang_DIR=$Clang_DIR \
-      -DBUILD_SHARED_LIBS=NO \
-      -DWITH_TESTS=NO -DWITH_APPS=NO -DWITH_TUTORIALS=NO \
-      -DWITH_DOCS=YES -DWITH_UTILS=NO -DWITH_PYTHON_BINDINGS=NO \
-      -S "$DIR" -B "$DIR/build/static"
+[ -z "$LLVM_DIR" ] && echo "Must set specific LLVM_DIR for packaging" && exit
 
-cmake -G "Ninja Multi-Config" \
-      -DLLVM_DIR=$LLVM_DIR -DClang_DIR=$Clang_DIR \
-      -DBUILD_SHARED_LIBS=YES \
-      -DWITH_TESTS=NO -DWITH_APPS=NO -DWITH_TUTORIALS=NO \
-      -DWITH_DOCS=YES -DWITH_UTILS=NO -DWITH_PYTHON_BINDINGS=NO \
-      -S "$DIR" -B "$DIR/build/shared"
+# shellcheck disable=SC2154
+[ -z "$Clang_DIR" ] && echo "Must set specific Clang_DIR for packaging" && exit
 
-cmake --build "$DIR/build/shared" --config Debug
-cmake --build "$DIR/build/shared" --config Release 
-cmake --build "$DIR/build/static" --config Debug
-cmake --build "$DIR/build/static" --config Release 
+# shellcheck disable=SC2034
+flag_shared="-DBUILD_SHARED_LIBS=YES"
 
-cmake --install "$DIR/build/shared" --prefix "$DIR/build/install" --config Debug
-cmake --install "$DIR/build/shared" --prefix "$DIR/build/install" --config Release 
-cmake --install "$DIR/build/static" --prefix "$DIR/build/install" --config Debug
-cmake --install "$DIR/build/static" --prefix "$DIR/build/install" --config Release
+# shellcheck disable=SC2034
+flag_static="-DBUILD_SHARED_LIBS=NO"
 
+for ty in shared static; do
+  for cfg in Debug Release; do
+    flag_name=flag_$ty
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=$cfg ${!flag_name} \
+          -DLLVM_DIR="$LLVM_DIR" -DClang_DIR="$Clang_DIR" \
+          -DWITH_TESTS=NO -DWITH_APPS=NO -DWITH_TUTORIALS=NO \
+          -DWITH_DOCS=YES -DWITH_UTILS=NO -DWITH_PYTHON_BINDINGS=NO \
+          -S "$DIR" -B "$DIR/build/$ty-$cfg"
+    cmake --build "$DIR/build/$ty-$cfg"
+  done
+done
+
+(
+  cd "$DIR/build" || exit
+  cpack --config "$DIR/packaging/Linux.cmake"
+)
